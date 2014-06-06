8b4778f2f82ba10431a5556c7278f8dc4bc851c5
Brent Gulanowski <brent@shopify.com>
2014-06-06 16:16:07 -0600

    New workspace and new demo from Chapter 2.

---
+++ Chapter02/Move/Move/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Move
+//
+//  Created by Brent Gulanowski on 2014-06-06.
+//  Copyright (c) 2014 GLSuperBible. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Chapter02/Move/MoveTests/MoveTests.m
@@ -0,0 +1,34 @@
+//
+//  MoveTests.m
+//  MoveTests
+//
+//  Created by Brent Gulanowski on 2014-06-06.
+//  Copyright (c) 2014 GLSuperBible. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoveTests : XCTestCase
+
+@end
+
+@implementation MoveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
