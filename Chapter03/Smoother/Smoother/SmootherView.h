eb0417cbcd80dc045a977b71a53569b2337450e2
Brent Gulanowski <brent@shopify.com>
2014-06-22 23:27:29 -0600

    Add Chapter 3, Smoother sample.

---
+++ Chapter03/Smoother/Smoother/SmootherView.h
@@ -0,0 +1,15 @@
+//
+//  SmootherView.h
+//  Smoother
+//
+//  Created by Brent Gulanowski on 2014-06-22.
+//  Copyright (c) 2014 GLSuperBible. All rights reserved.
+//
+
+#import "OGLSBView.h"
+
+@interface SmootherView : OGLSBView
+
+- (IBAction)menuSelection:(NSMenuItem *)sender;
+
+@end
